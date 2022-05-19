d0818ecee6680897805ef2a6312e717f6283ef6d
Linh <nguyentanlinh90@gmail.com>
2022-05-19 14:45:55 +0700

    receiver  broadcastreceiver with object and list object

--- app/src/main/java/com/ntl/guidelinesappreceiver/MainActivity.java
+++ app/src/main/java/com/ntl/guidelinesappreceiver/MainActivity.java
@@ -9,10 +9,22 @@ import android.content.IntentFilter;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     private String MY_BROADCAST_ACTION = "com.ntl.MY_ACTION";
+    private String MY_BROADCAST_OBJECT_ACTION = "com.ntl.MY_OBJECT_ACTION";
     private String MY_KEY = "my_key";
-    private TextView tvReceiver;
+    private String MY_KEY_LIST_OBJECT = "my_key_list_object";
+
+    private TextView tvReceiver, tvReceiverObject;
 
     private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
         @Override
@@ -20,6 +32,35 @@ public class MainActivity extends AppCompatActivity {
             if (MY_BROADCAST_ACTION.equals(intent.getAction())) {
                 String strText = intent.getStringExtra(MY_KEY);
                 tvReceiver.setText(strText);
+            }
+
+            if (MY_BROADCAST_OBJECT_ACTION.equals(intent.getAction())) {
+                Gson gson = new Gson();
+
+                //todo case 1: get OBJECT
+                String strUser = intent.getStringExtra(MY_KEY);
+                User user = gson.fromJson(strUser, User.class);
+
+                //todo case 2: get LIST OBJECT with logic below
+                String strJson = intent.getStringExtra(MY_KEY_LIST_OBJECT);
+                List<User> list = new ArrayList<>();
+                try {
+                    JSONArray jsonArray = new JSONArray(strJson);
+                    JSONObject jsonObject;
+                    User user1;
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        jsonObject = jsonArray.getJSONObject(i);
+                        user1 = gson.fromJson(jsonObject.toString(), User.class);
+                        list.add(user1);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+                tvReceiverObject.setText(user.getName() + " : " + user.getAddress() + "\n" +
+                        "size list = " + list.size());
+
             }
         }
     };
@@ -30,6 +71,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         tvReceiver = findViewById(R.id.tv_receiver_from_broadcast_guidelines_app);
+        tvReceiverObject = findViewById(R.id.tv_receiver_from_broadcast_guidelines_app_object);
     }
 
     @Override
@@ -37,6 +79,9 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
         IntentFilter intentFilter = new IntentFilter(MY_BROADCAST_ACTION);
         registerReceiver(broadcastReceiver, intentFilter);
+
+        IntentFilter intentFilterObject = new IntentFilter(MY_BROADCAST_OBJECT_ACTION);
+        registerReceiver(broadcastReceiver, intentFilterObject);
     }
 
     @Override
---
+++ app/src/main/java/com/ntl/guidelinesappreceiver/MyBroadcastReceiver1.java
@@ -0,0 +1,14 @@
+package com.ntl.guidelinesappreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class MyBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String text = intent.getStringExtra(MainActivity.MY_KEY);
+        Toast.makeText(context, "MyBroadcastReceiver1 " + text, Toast.LENGTH_SHORT).show();
+    }
+}
